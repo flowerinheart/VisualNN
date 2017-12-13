flowerinheart/VisualNN

commit fbd6128bef1ed3dc837533030e7fd47863db2ba0

On click, a new layer connects to bottom of node if possible. (#174)
* onKeyUp Event

* Fixed button click

* Event

* Connection

* When clicked on pane, new layer is connected to last node

* Requested changes

* Case for handling target layer being a loss layer

* Removed clicker, added comments

ide/static/js/canvas.js
@@ -35,8 +35,8 @@ class Canvas extends React.Component {
         grid: [8, 8]
       }
     );
+    const net = this.props.net;    
     if (this.props.rebuildNet) {
-      const net = this.props.net;
       let combined_layers = ['ReLU', 'LRN', 'TanH', 'BatchNorm', 'Dropout', 'Scale'];
       Object.keys(net).forEach(inputId => {
         const layer = net[inputId];
@@ -69,6 +69,24 @@ class Canvas extends React.Component {
       });
       this.props.changeNetStatus(false);
       // instance.repaintEverything();
+    }
+    if(Object.keys(net).length>1){
+      const x1 = parseInt(net[`l${this.props.nextLayerId-2}`].state.top.split('px'));
+      const x2 = parseInt(net[`l${this.props.nextLayerId-1}`].state.top.split('px')); 
+      const s = instance.getEndpoints(`l${this.props.nextLayerId-2}`)[0];
+      var t = instance.getEndpoints(`l${this.props.nextLayerId-1}`);
+      // To handle case of loss layer being target
+      if (t.length == 1){
+        t = t[0];
+      }
+      else{
+        t = t[1];
+      }
+      if (x2-x1==80) { //since only layers added through handleClick will be exactly 80px apart, we can connect those like this.
+        instance.connect({
+          source: s,
+          target: t});
+      }
     }
   }
   allowDrop(event) {
@@ -301,7 +319,7 @@ class Canvas extends React.Component {
 Canvas.propTypes = {
   nextLayerId: React.PropTypes.number,
   selectedPhase: React.PropTypes.number,
-  net: React.PropTypes.object,
+  net: React.PropTypes.object.isRequired,
   modifyLayer: React.PropTypes.func,
   addNewLayer: React.PropTypes.func,
   changeSelectedLayer: React.PropTypes.func,

ide/static/js/content.js
@@ -9,6 +9,7 @@ import data from './data';
 import netLayout from './netLayout_vertical';
 import Modal from 'react-modal';
 import ModelZoo from './modelZoo';
+import $ from 'jquery'
 
 const infoStyle = {
   content : {
@@ -66,6 +67,7 @@ class Content extends React.Component {
     this.zooModal = this.zooModal.bind(this);
     this.modalContent = null;
     this.modalHeader = null;
+    this.handleClick = this.handleClick.bind(this);
   }
   openModal() {
     this.setState({modalIsOpen: true});
@@ -680,6 +682,71 @@ class Content extends React.Component {
     this.modalContent = <ModelZoo importNet={this.importNet}/>;
     this.openModal();
   }
+  
+  handleClick(event) {
+    event.preventDefault();
+
+    const net = this.state.net;
+    const id = event.target.id;
+    const prev = net[`l${this.state.nextLayerId-1}`];
+    const next = data[id];
+    const zoom = instance.getZoom();    
+    const layer = {};
+    let phase = this.state.selectedPhase;
+    
+    if (this.state.nextLayerId>0 //makes sure that there are other layers 
+      &&data[prev.info.type].endpoint.src == "Bottom" //makes sure that the source has a bottom
+      &&next.endpoint.trg == "Top") { //makes sure that the target has a top
+        layer.connection = { input: [], output: [] };
+        layer.info = {
+          type: id.toString(),
+          phase,
+          class: ''
+        }
+        layer.params = {
+          'endPoint' : [next['endpoint'], false] //This key is endpoint in data.js, but endPoint in everywhere else.
+        }          
+        Object.keys(next.params).forEach(j => {
+          layer.params[j] = [next.params[j].value, false]; //copys all params from data.js
+        });    
+        layer.props = JSON.parse(JSON.stringify(next.props)) //copys all props rom data.js
+        layer.state = {
+          top: `${(parseInt(prev.state.top.split('px')[0])/zoom + 80)}px`, // This makes the new layer is exactly 80px under the previous one.
+          left: `${(parseInt(prev.state.left.split('px')[0])/zoom)}px`, // This aligns the new layer with the previous one.
+          class: '' 
+        }
+        layer.props.name = `${next.name}${this.state.nextLayerId}`;          
+        this.addNewLayer(layer);
+    }
+
+    else if (Object.keys(net).length == 0) { // if there are no layers
+      layer.connection = { input: [], output: [] };
+      layer.info = {
+            type: id.toString(),
+            phase,
+            class: ''
+          }
+      layer.params = {
+        'endPoint' : [next['endpoint'], false] //This key is endpoint in data.js, but endPoint in everywhere else.
+      }          
+      Object.keys(next.params).forEach(j => {
+        layer.params[j] = [next.params[j].value, false];  //copys all params from data.js
+      });    
+      layer.props = JSON.parse(JSON.stringify(next.props)) //copys all props from data.js
+      const height = Math.round(0.05*window.innerHeight, 0); // 5% of screen height, rounded to zero decimals
+      const width = Math.round(0.35*window.innerWidth, 0); // 35% of screen width, rounded to zero decimals
+      var top = height + Math.ceil(41-height);
+      var left = width;
+      layer.state = {
+            top: `${top}px`,
+            left: `${left}px`,
+            class: '' 
+          }
+      layer.props.name = `${next.name}${this.state.nextLayerId}`;          
+      this.addNewLayer(layer); 
+    }
+
+  }
   render() {
     let loader = null;
     if (this.state.load) {
@@ -703,7 +770,9 @@ class Content extends React.Component {
               zooModal={this.zooModal}
              />
              <h5 className="sidebar-heading">INSERT LAYER</h5>
-             <Pane />
+             <Pane 
+             handleClick = {this.handleClick}
+             />
              <div className="text-center">
               <Tabs selectedPhase={this.state.selectedPhase} changeNetPhase={this.changeNetPhase} />
              </div>

ide/static/js/pane.js
@@ -36,15 +36,15 @@ class Pane extends React.Component {
                 </div>
                 <div id="data" className="panel-collapse collapse" role=" tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="ImageData">Image Data</PaneElement>
-                    <PaneElement id="Data">Data</PaneElement>
-                    <PaneElement id="HDF5Data">HDF5 Data</PaneElement>
-                    <PaneElement id="HDF5Output">HDF5 Output</PaneElement>
-                    <PaneElement id="Input">Input</PaneElement>
-                    <PaneElement id="WindowData">Window Data</PaneElement>
-                    <PaneElement id="MemoryData">Memory Data</PaneElement>
-                    <PaneElement id="DummyData">Dummy Data</PaneElement>
-                    <PaneElement id="Python">Python</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="ImageData">Image Data</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Data">Data</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="HDF5Data">HDF5 Data</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="HDF5Output">HDF5 Output</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Input">Input</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="WindowData">Window Data</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="MemoryData">Memory Data</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="DummyData">Dummy Data</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Python">Python</PaneElement>
                   </div>
                 </div>
               </div>
@@ -59,13 +59,13 @@ class Pane extends React.Component {
                 </div>
                 <div id="vision" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="Convolution">Convolution</PaneElement>
-                    <PaneElement id="Pooling">Pool</PaneElement>
-                    <PaneElement id="Upsample">Upsample</PaneElement>
-                    <PaneElement id="LocallyConnected">Locally Connected</PaneElement>
-                    <PaneElement id="Crop">Crop</PaneElement>
-                    <PaneElement id="SPP">SPP</PaneElement>
-                    <PaneElement id="Deconvolution">Deconvolution</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Convolution">Convolution</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Pooling">Pool</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Upsample">Upsample</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="LocallyConnected">Locally Connected</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Crop">Crop</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="SPP">SPP</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Deconvolution">Deconvolution</PaneElement>
                   </div>
                 </div>
               </div>
@@ -80,10 +80,10 @@ class Pane extends React.Component {
                 </div>
                 <div id="recurrent" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="Recurrent">Recurrent</PaneElement>
-                    <PaneElement id="RNN">RNN</PaneElement>
-                    <PaneElement id="GRU">GRU</PaneElement>
-                    <PaneElement id="LSTM">LSTM</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Recurrent">Recurrent</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="RNN">RNN</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="GRU">GRU</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="LSTM">LSTM</PaneElement>
                   </div>
                 </div>
               </div>
@@ -98,21 +98,21 @@ class Pane extends React.Component {
                 </div>
                 <div id="utility" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="Flatten">Flatten</PaneElement>
-                    <PaneElement id="Reshape">Reshape</PaneElement>
-                    <PaneElement id="BatchReindex">Batch Reindex</PaneElement>
-                    <PaneElement id="Split">Split</PaneElement>
-                    <PaneElement id="Concat">Concat</PaneElement>
-                    <PaneElement id="Eltwise">Eltwise</PaneElement>
-                    <PaneElement id="Filter">Filter</PaneElement>
-                    <PaneElement id="Reduction">Reduction</PaneElement>
-                    <PaneElement id="Silence">Silence</PaneElement>
-                    <PaneElement id="ArgMax">ArgMax</PaneElement>
-                    <PaneElement id="Softmax">Softmax</PaneElement>
-                    <PaneElement id="Permute">Permute</PaneElement>
-                    <PaneElement id="RepeatVector">Repeat Vector</PaneElement>
-                    <PaneElement id="Regularization">Regularization</PaneElement>
-                    <PaneElement id="Masking">Masking</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Flatten">Flatten</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Reshape">Reshape</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="BatchReindex">Batch Reindex</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Split">Split</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Concat">Concat</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Eltwise">Eltwise</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Filter">Filter</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Reduction">Reduction</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Silence">Silence</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="ArgMax">ArgMax</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Softmax">Softmax</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Permute">Permute</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="RepeatVector">Repeat Vector</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Regularization">Regularization</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Masking">Masking</PaneElement>
                   </div>
                 </div>
               </div>
@@ -127,24 +127,24 @@ class Pane extends React.Component {
                 </div>
                 <div id="activation" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="ReLU">ReLU/Leaky-ReLU</PaneElement>
-                    <PaneElement id="PReLU">PReLU</PaneElement>
-                    <PaneElement id="ELU">ELU</PaneElement>
-                    <PaneElement id="ThresholdedReLU">Thresholded ReLU</PaneElement>
-                    <PaneElement id="SELU">SELU</PaneElement>
-                    <PaneElement id="Softplus">Softplus</PaneElement>
-                    <PaneElement id="Softsign">Softsign</PaneElement>
-                    <PaneElement id="Sigmoid">Sigmoid</PaneElement>
-                    <PaneElement id="TanH">TanH</PaneElement>
-                    <PaneElement id="HardSigmoid">Hard Sigmoid</PaneElement>
-                    <PaneElement id="AbsVal">Absolute Value</PaneElement>
-                    <PaneElement id="Power">Power</PaneElement>
-                    <PaneElement id="Exp">Exp</PaneElement>
-                    <PaneElement id="Log">Log</PaneElement>
-                    <PaneElement id="BNLL">BNLL</PaneElement>
-                    <PaneElement id="Threshold">Threshold</PaneElement>
-                    <PaneElement id="Bias">Bias</PaneElement>
-                    <PaneElement id="Scale">Scale</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="ReLU">ReLU/Leaky-ReLU</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="PReLU">PReLU</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="ELU">ELU</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="ThresholdedReLU">Thresholded ReLU</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="SELU">SELU</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Softplus">Softplus</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Softsign">Softsign</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Sigmoid">Sigmoid</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="TanH">TanH</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="HardSigmoid">Hard Sigmoid</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="AbsVal">Absolute Value</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Power">Power</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Exp">Exp</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Log">Log</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="BNLL">BNLL</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Threshold">Threshold</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Bias">Bias</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Scale">Scale</PaneElement>
                   </div>
                 </div>
               </div>
@@ -159,9 +159,9 @@ class Pane extends React.Component {
                 </div>
                 <div id="normalization" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="LRN">LRN</PaneElement>
-                    <PaneElement id="MVN">MVN</PaneElement>
-                    <PaneElement id="BatchNorm">Batch Norm</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="LRN">LRN</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="MVN">MVN</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="BatchNorm">Batch Norm</PaneElement>
                   </div>
                 </div>
               </div>
@@ -176,9 +176,9 @@ class Pane extends React.Component {
                 </div>
                 <div id="common" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="InnerProduct">Inner Product</PaneElement>
-                    <PaneElement id="Dropout">Dropout</PaneElement>
-                    <PaneElement id="Embed">Embed</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="InnerProduct">Inner Product</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Dropout">Dropout</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Embed">Embed</PaneElement>
                   </div>
                 </div>
               </div>
@@ -193,9 +193,9 @@ class Pane extends React.Component {
                 </div>
                 <div id="noise" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="GaussianNoise">Gaussian Noise</PaneElement>
-                    <PaneElement id="GaussianDropout">Gaussian Dropout</PaneElement>
-                    <PaneElement id="AlphaDropout">Alpha Dropout</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="GaussianNoise">Gaussian Noise</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="GaussianDropout">Gaussian Dropout</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="AlphaDropout">Alpha Dropout</PaneElement>
                   </div>
                 </div>
               </div>
@@ -210,15 +210,15 @@ class Pane extends React.Component {
                 </div>
                 <div id="loss" className="panel-collapse collapse" role="tabpanel">
                   <div className="panel-body">
-                    <PaneElement id="MultinomialLogisticLoss">Multinomial Logistic Loss</PaneElement>
-                    <PaneElement id="InfogainLoss">Infogain Loss</PaneElement>
-                    <PaneElement id="SoftmaxWithLoss">Softmax With Loss</PaneElement>
-                    <PaneElement id="EuclideanLoss">Euclidean Loss</PaneElement>
-                    <PaneElement id="HingeLoss">Hinge Loss</PaneElement>
-                    <PaneElement id="SigmoidCrossEntropyLoss">Sigmoid Cross Entropy Loss</PaneElement>
-                    <PaneElement id="Accuracy">Accuracy</PaneElement>
-                    <PaneElement id="ContrastiveLoss">Contrastive Loss</PaneElement>
-                    <PaneElement id="Python">Python</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="MultinomialLogisticLoss">Multinomial Logistic Loss</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="InfogainLoss">Infogain Loss</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="SoftmaxWithLoss">Softmax With Loss</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="EuclideanLoss">Euclidean Loss</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="HingeLoss">Hinge Loss</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="SigmoidCrossEntropyLoss">Sigmoid Cross Entropy Loss</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Accuracy">Accuracy</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="ContrastiveLoss">Contrastive Loss</PaneElement>
+                    <PaneElement handleClick={this.props.handleClick} id="Python">Python</PaneElement>
                   </div>
                 </div>
               </div>
@@ -228,5 +228,7 @@ class Pane extends React.Component {
       );
   }
 }
-
+Pane.propTypes = {
+  handleClick: React.PropTypes.func.isRequired
+};
 export default Pane;

ide/static/js/paneElement.js
@@ -13,6 +13,7 @@ class PaneElement extends React.Component {
       <div
         className="btn btn-block drowpdown-button"
         draggable="true"
+        onClickCapture={this.props.handleClick}        
         onDragStart={this.drag}
         id={this.props.id}
       >
@@ -24,7 +25,8 @@ class PaneElement extends React.Component {
 
 PaneElement.propTypes = {
   id: React.PropTypes.string.isRequired,
-  children: React.PropTypes.string.isRequired
+  children: React.PropTypes.string.isRequired,
+  handleClick: React.PropTypes.func.isRequired
 };
 
 export default PaneElement;

commit cb1844db83256a85cc73d62935d500f3e405e6d9

Bug fix for issue caused by #174
* Trying to access layers which are not present
* Added restrictions for running that function
* Might need to improve this logic later

ide/static/js/canvas.js
@@ -70,7 +70,8 @@ class Canvas extends React.Component {
       this.props.changeNetStatus(false);
       // instance.repaintEverything();
     }
-    if(Object.keys(net).length>1){
+    // Might need to improve the logic of clickEvent
+    if(Object.keys(net).length>1 && this.props.clickEvent){
       const x1 = parseInt(net[`l${this.props.nextLayerId-2}`].state.top.split('px'));
       const x2 = parseInt(net[`l${this.props.nextLayerId-1}`].state.top.split('px')); 
       const s = instance.getEndpoints(`l${this.props.nextLayerId-2}`)[0];
@@ -329,7 +330,8 @@ Canvas.propTypes = {
   addError: React.PropTypes.func,
   dismissError: React.PropTypes.func,
   error: React.PropTypes.array,
-  placeholder: React.PropTypes.bool
+  placeholder: React.PropTypes.bool,
+  clickEvent: React.PropTypes.bool
 };
 
 export default Canvas;

ide/static/js/content.js
@@ -67,6 +67,8 @@ class Content extends React.Component {
     this.zooModal = this.zooModal.bind(this);
     this.modalContent = null;
     this.modalHeader = null;
+    // Might need to improve the logic of clickEvent
+    this.clickEvent = false;
     this.handleClick = this.handleClick.bind(this);
   }
   openModal() {
@@ -244,6 +246,7 @@ class Content extends React.Component {
   importNet(framework, id) {
     this.dismissAllErrors();
     this.closeModal();
+    this.clickEvent = false;
     const url = {'caffe': '/caffe/import', 'keras': '/keras/import', 'tensorflow': '/tensorflow/import'};
     const formData = new FormData();
     const caffe_fillers = ['constant', 'gaussian', 'positive_unitball', 'uniform', 'xavier', 'msra', 'bilinear'];
@@ -685,6 +688,7 @@ class Content extends React.Component {
   
   handleClick(event) {
     event.preventDefault();
+    this.clickEvent = true;
 
     const net = this.state.net;
     const id = event.target.id;
@@ -745,7 +749,6 @@ class Content extends React.Component {
       layer.props.name = `${next.name}${this.state.nextLayerId}`;          
       this.addNewLayer(layer); 
     }
-
   }
   render() {
     let loader = null;
@@ -797,6 +800,7 @@ class Content extends React.Component {
             error={this.state.error}
             dismissError={this.dismissError}
             addError={this.addError}
+            clickEvent={this.clickEvent}
           />
           <SetParams
             net={this.state.net}